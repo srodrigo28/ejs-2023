c2bbac0614db4c3ad419323259d09844e553a8f9
Sebastiao Rodrigo <rodrigoexer1@gmail.com>
2023-10-06 21:51:27 -0300

    updated

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
-const app = expres();
+const app = express();
 
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Teste</h1>')
+    //res.send('<h1>Teste</h1>')
+    res.sendFile('./views/home.html', { root: __dirname})
 })
 
 app.get('/about', (req, res) => {
--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
+* Link Exemplo
 ```
 https://www.youtube.com/watch?v=tMy2oeiDhDE&list=PLXik_5Br-zO8Ul2-XnkTf99jiYaDylAAW&index=25
+```
+
+* Npm ejs
+```
+https://www.npmjs.com/package/ejs?activeTab=readme
+```
+
+* Ejs Doc
+```
+https://ejs.co/#install
 ```
