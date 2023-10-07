601d80c2a3adf942926ff4a356b6832d28bdcce9
Sebastiao Rodrigo <rodrigoexer1@gmail.com>
2023-10-07 06:41:44 -0300

    updated

--- app.js
+++ app.js
@@ -6,9 +6,21 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
     //res.send('<h1>Teste</h1>')
-    res.sendFile('./views/home.html', { root: __dirname})
+    res.status(502).redirect('./views/home.html', { root: __dirname})
 })
-
+app.get('/home', (req, res) => {
+    //res.send('<h1>Teste</h1>')
+    res.status(200).sendFile('./views/home.html', { root: __dirname})
+})
 app.get('/about', (req, res) => {
-    res.send('<h1>Sobre</h1>')
+    //res.send('<h1>Teste</h1>')
+    res.status(200).sendFile('./views/about.html', { root: __dirname})
+})
+app.get('/service', (req, res) => {
+    //res.send('<h1>Teste</h1>')
+    res.status(200).sendFile('./views/service.html', { root: __dirname})
+})
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname})
+    //res.redirect('./views/service.html', { root: __dirname})
 })
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 * Link Exemplo
 ```
-https://www.youtube.com/watch?v=tMy2oeiDhDE&list=PLXik_5Br-zO8Ul2-XnkTf99jiYaDylAAW&index=25
+https://www.youtube.com/watch?v=d9W3xgaOIbM&list=PLXik_5Br-zO8Ul2-XnkTf99jiYaDylAAW&index=28
 ```
 
 * Npm ejs
